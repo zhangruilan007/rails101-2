7149b6fc8256db7557fea0c7bbdc7ce82521bd21
张瑞兰 <zhangruilan007@gmail.com>
2017-01-08 00:17:00 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -47,7 +47,9 @@ def destroy
   def find_group_and_check_permission
     @group = Group.find(params[:id])
 
-    if current_user root_path, alert: "You have no permission."
+    if current_user != @group.user
+
+    redirect_to root_path, alert: "You have no permission."
     end
   end
 
